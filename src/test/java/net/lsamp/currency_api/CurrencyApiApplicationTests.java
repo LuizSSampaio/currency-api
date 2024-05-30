46ac66881ec57d33b5fa445a622f05267076ba59
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2024-05-30 14:19:22 -0300

    feat: initial commit

---
+++ src/test/java/net/lsamp/currency_api/CurrencyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.lsamp.currency_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
